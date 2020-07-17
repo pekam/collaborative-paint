ad0447728ed5d9ef694cf413ba68a94a8eabb68b
Pekka Maanpää <pekkamaa@vaadin.com>
2020-07-17 19:27:39 +0300

    Add canvas and implement painting

--- frontend/views/paint/paint-view.ts
+++ frontend/views/paint/paint-view.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css, customElement } from 'lit-element';
+import {LitElement, html, css, customElement, query} from 'lit-element';
 
 @customElement('paint-view')
 export class PaintView extends LitElement {
@@ -7,13 +7,55 @@ export class PaintView extends LitElement {
       :host {
         display: block;
       }
+      #canvas {
+        border: 3px solid white;
+        border-radius: 10px;
+      }
     `;
+  }
+
+  private readonly WIDTH: number = 1000;
+  private readonly HEIGHT: number = 1000;
+
+  @query('#canvas')
+  private canvas: any;
+  private ctx: any;
+
+  private mouseX: number = 0;
+  private mouseY: number = 0;
+
+  async firstUpdated(changedProperties: any) {
+    super.firstUpdated(changedProperties);
+    this.ctx = this.canvas.getContext('2d');
   }
 
   render() {
     return html`
-      <br />
-      Content placeholder
+      <canvas id="canvas"
+        width=${this.WIDTH}
+        height=${this.HEIGHT}
+        @mousemove="${this.onMousemove}"
+      ></canvas>
     `;
   }
+
+  private onMousemove = (e: MouseEvent) => {
+    if (e.buttons === 1) {
+      const {x: offsetX, y: offsetY} =
+          this.canvas.getBoundingClientRect();
+      const {clientX: mouseX, clientY: mouseY} = e;
+      this.ctx.strokeStyle = 'white';
+      this.ctx.lineWidth = 10;
+      this.ctx.lineCap = "round";
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.mouseX - offsetX,
+          this.mouseY - offsetY);
+      this.ctx.lineTo(mouseX - offsetX,
+          mouseY - offsetY);
+      this.ctx.stroke();
+    }
+    this.mouseX = e.clientX;
+    this.mouseY = e.clientY;
+  }
+
 }
